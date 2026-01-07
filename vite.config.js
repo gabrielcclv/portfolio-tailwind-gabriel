bd5433d0be02d46a979809d2298e1429ac820bf9
Gabriel <gabriel.calvo@outlook.com>
2026-01-07 11:28:10 +0100

    Correcion de la configuracion de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss()],
-	base: process.env.NODE_ENV === 'production' ? '/portafolio-tailwind/' : '/',
+	base: process.env.NODE_ENV === 'production' ? '/portafolio-tailwind-gabriel/' : '/',
 	build: {
 		outDir: 'dist',
 		assetsDir: 'assets',
