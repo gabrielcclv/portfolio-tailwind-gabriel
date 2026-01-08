92dbd7d48034c137d29aaf69d7f9ffc4e91afd13
Gabriel <gabriel.calvo@outlook.com>
2026-01-08 23:20:57 +0100

    Ultimos retoques

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Sustituye estos valores por los que más te gusten (ej. un azul eléctrico o un verde neón)
+        
         brand: {
-          light: '#00d2ff', // Un color vibrante para acentos
+          light: '#00d2ff',
           DEFAULT: '#0072ff',
           dark: '#003366',
         },
@@ -17,7 +17,7 @@ export default {
           900: '#0f172a', // Para el modo oscuro
         }
       },
-      // Añadimos una fuente personalizada si quieres
+
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
         display: ['Lexend', 'sans-serif'], // Para titulares
